9429c61ccb60dcddfc888dd316d2f9694a254834
Andrew Ingraham <andrewingraham@Andrews-MacBook-Air-3.local>
2019-05-26 20:36:26 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+/* global $ */
+
+
